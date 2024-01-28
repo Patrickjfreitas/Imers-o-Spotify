f48ac9a87ac8b8b7f90ca61ee1599d0264186b55
Patrickjfreitas <129640193+PatrickFrontend@users.noreply.github.com>
2024-01-28 01:12:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia Familia !"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde Familia !"
+    : "Boa noite Familia !";
+
+
+greetingElement.textContent = greetingMessage;
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => { 
+  const containerWidth = container.offsetWidth; 
+  const numColumns = Math.floor(containerWidth / 200); 
+
+
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+  console.log({ container });
+  console.log({ numColumns });
+});
+
+observer.observe(container);
+
